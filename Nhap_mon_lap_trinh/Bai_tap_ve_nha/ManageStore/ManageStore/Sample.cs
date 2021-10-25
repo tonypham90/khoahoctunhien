39551de9ec940cbe466ebd9c10dc35c75cd9bae7
tonypham90 <tuananh.gt@hotmail.com>
2021-10-25 23:19:58 +0700

    fix 1st create item

--- Nhap_mon_lap_trinh/Bai_tap_ve_nha/ManageStore/ManageStore/Sample.cs
+++ Nhap_mon_lap_trinh/Bai_tap_ve_nha/ManageStore/ManageStore/Sample.cs
@@ -33,7 +33,6 @@ namespace ManageStore
         {
             string newid = null;
             bool duplicateCheck = true;
-            int count = 2;
             while (duplicateCheck)
             {
                 newid = new string(Enumerable.Repeat(Chars, 4).Select(s => s[ran.Next(s.Length)]).ToArray());
