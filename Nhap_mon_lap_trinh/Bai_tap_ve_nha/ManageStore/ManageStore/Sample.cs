7342b05e328841d23b8cb1f84da2ade2bf12e667
tonypham90 <tuananh.gt@hotmail.com>
2021-10-26 11:01:14 +0700

    update limit

--- Nhap_mon_lap_trinh/Bai_tap_ve_nha/ManageStore/ManageStore/Sample.cs
+++ Nhap_mon_lap_trinh/Bai_tap_ve_nha/ManageStore/ManageStore/Sample.cs
@@ -1,18 +1,10 @@
 using System;
-<<<<<<< HEAD
 using System.Linq;
 
-=======
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Data;
-using System.Linq;
->>>>>>> a7b2030 (Input item rule)
 namespace ManageStore
 {
     public class Sample
     {
-<<<<<<< HEAD
         private const string Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         private static readonly Random ran = new();
 
@@ -50,44 +42,6 @@ namespace ManageStore
             return detailValue;
         }
 
-=======
-        private static Random ran = new Random();
-        const string Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        
-
-        private protected static string[] Food = new string[] 
-            { "Chuối".ToUpper(), "Sữa", "Trứng", "Cà phê", "Mì gói"};
-        private protected static string[] Electric = new string[]
-            { "Điện Thoại Di Động", "Labtop", "Tủ lạnh", "Tivi", "Điều hòa nhiệt độ" };
-        private protected static string[] Equitment = new string[] 
-            { "Chổi", "chén", "máy hút bụi", "cây lau nhà", "bùi nhùi" };
-        private protected static string[] Brand = new string[] 
-            { "Daklak", "Intel", "Sony", "X-Space", "Takana","Viet-Gab" };
-        private protected static string[] Type = new string[] 
-            { "THUCPHAM", "DIENTU", "GIADUNG" };
-        
-        
-        
-        /*public static string RanId(List<Item> items)
-        {
-            string newid;
-            bool duplicatecheck = true;
-            while (true)
-            {
-                
-            }
-            return new string(Enumerable.Repeat(Chars, 4).Select(s => s[ran.Next(s.Length)]).ToArray());
-        }*/
-
-        public static string RandItemDetail(string[]detail)
-        {
-            int id = ran.Next(0, detail.Length);
-            string detailValue = detail[id].ToUpper();
-            Console.WriteLine(detailValue);
-            return detailValue;
-        }
-        
->>>>>>> a7b2030 (Input item rule)
         public static string RandName(string lable)
         {
             string ItemName;
@@ -104,10 +58,7 @@ namespace ManageStore
                     item = Equitment;
                     break;
             }
-<<<<<<< HEAD
 
-=======
->>>>>>> a7b2030 (Input item rule)
             ItemName = item[ran.Next(0, item.Length)].ToUpper();
             Console.WriteLine(ItemName);
             return ItemName;
@@ -116,7 +67,6 @@ namespace ManageStore
         public static Date RandDate(Date min)
         {
             Date value;
-<<<<<<< HEAD
             value.Year = ran.Next(min.Year, min.Year + 6);
             if (value.Year > min.Year)
                 value.Month = ran.Next(1, 13);
@@ -138,34 +88,11 @@ namespace ManageStore
             
             Console.WriteLine(note);
             packaged.Id = RanId(warehouse.ItemsList);
-=======
-            value.Year = ran.Next(min.Year, min.Year+6);
-            if (value.Year>min.Year)
-            {
-                value.Month = ran.Next(1, 13);
-            }
-            else
-            { 
-                value.Month = ran.Next(min.Month,13);
-            }
-            return value;
-        }
-
-        public static Item RandItem(string note,List<Item> items,List<string>lable)
-        {
-            Item packaged;
-            Console.WriteLine(note);
-            packaged.Id = RanId(items);
->>>>>>> a7b2030 (Input item rule)
             packaged.Type = RandItemDetail(Type);
             packaged.Name = RandName(packaged.Type);
             packaged.Qty = ran.Next(1, 100);
-            packaged.Mfg.Month = ran.Next(13);
-<<<<<<< HEAD
+            packaged.Mfg.Month = ran.Next(1,13);
             packaged.Mfg.Year = ran.Next(19, 21);
-=======
-            packaged.Mfg.Year = ran.Next(19,21);
->>>>>>> a7b2030 (Input item rule)
             packaged.Exp = RandDate(packaged.Mfg);
             packaged.Com = RandItemDetail(Brand);
 
