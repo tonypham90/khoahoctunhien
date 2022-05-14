f527c327bc09be2647bc9b115e26c29b085703e7
tonypham90 <tuananh.gt@hotmail.com>
2022-05-15 00:05:26 +0700

    New Update
    Fix crack

---
+++ Ky_Thuat_Lap_Trinh/21880005_DoAn_KTLT/Manage_Store/Manage_Store/Pages/CreateItemType.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Manage_Store.Pages;
+
+public class CreateItemType : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
--- Ky_Thuat_Lap_Trinh/Training/Practive_KTLT/Practive_KTLT/DAL/Store_Item.cs
+++ Ky_Thuat_Lap_Trinh/Training/Practive_KTLT/Practive_KTLT/DAL/Store_Item.cs
@@ -3,12 +3,12 @@ using Practive_KTLT.Service;
 
 namespace Practive_KTLT.DAL;
 
-public class Store_Item
+public class StoreItem
 {
-    public static string CurrentDir = Environment.CurrentDirectory;
-    public static DirectoryInfo directory = new DirectoryInfo(CurrentDir);
-    public static FileInfo storedata = new FileInfo("ProductStore.json");
-    public static FileInfo ItemType = new FileInfo("ItemType.json");
+    private static string CurrentDir = Environment.CurrentDirectory;
+    private static readonly DirectoryInfo Directory = new DirectoryInfo(CurrentDir);
+    public static readonly FileInfo StoreLFileInfo = new FileInfo("ProductStore.json");
+    private static readonly FileInfo ItemTypeFileInfo = new FileInfo("ItemType.json");
     public static bool Add_Item(Item s)
     {
         List<Item> productList = DownloadItemData();
@@ -18,10 +18,10 @@ public class Store_Item
 
     public static List<Item> DownloadItemData()
     {
-        string dir = directory.FullName;
-        StreamReader Read_data = new StreamReader(storedata.FullName);
-        string jsonstring = Read_data.ReadToEnd();
-        Read_data.Close();
+        string dir = Directory.FullName;
+        StreamReader readData = new StreamReader(StoreLFileInfo.FullName);
+        string jsonstring = readData.ReadToEnd();
+        readData.Close();
         List<Item> itemData = JsonConvert.DeserializeObject<List<Item>>(jsonstring);
         return itemData;
     }
@@ -32,9 +32,9 @@ public class Store_Item
         {
             return false;
         }
-        StreamWriter Write_data = new StreamWriter(directory.FullName + storedata.FullName);
+        StreamWriter writeData = new StreamWriter(StoreLFileInfo.FullName);
         string jsonString = JsonConvert.SerializeObject(itemData);
-        Write_data.Write(jsonString);
+        writeData.Write(jsonString);
         return true;
     }
     
